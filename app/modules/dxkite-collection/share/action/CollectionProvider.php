ae5888955a546e3794a9faa3137ba1e08a424b31
DXkite <dxkite@163.com>
2018-04-22 09:52:26 +0800

    添加：次数统计

--- app/modules/dxkite-collection/share/action/CollectionProvider.php
+++ app/modules/dxkite-collection/share/action/CollectionProvider.php
@@ -6,7 +6,10 @@ class CollectionProvider
 {
    public static function getCollectionInfo() {
         $table =new Collection;
-        $row=$table->query('SELECT count( DISTINCT  device) as number FROM #{@table@}#;')->fetch();
-        return ['device'=>$row['number']];
+        $row=$table->query('SELECT count(DISTINCT  device) as number FROM #{@table@}#;')->fetch();
+        $boot=$table->query('SELECT count(device) as boot FROM #{@table@}#;')->fetch();
+        $ipTimes=$table->query('SELECT count(DISTINCT ip) as ipTimes FROM #{@table@}#;')->fetch();
+
+        return ['device'=>$row['number'],'boot'=>$boot['boot'],'ipTimes'=>$ipTimes['ipTimes']];
    }
 }
